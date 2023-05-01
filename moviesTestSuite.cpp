7ff56ac92d879de2b5b8b404a0bedfd05777bb2d
ian <77030032+yanathan@users.noreply.github.com>
2023-05-01 10:02:55 -0700

    Update moviesTestSuite.cpp

--- moviesTestSuite.cpp
+++ moviesTestSuite.cpp
@@ -5,6 +5,7 @@
 #include "Person.h"
 // Tests
 TEST(MovieTest, getTitle) {
+    //      Movie Title, Vector String of actor names in movie, Director name string, release date string, genre string, ID int
     Movie m("The Shawshank Redemption", {"Tim Robbins", "Morgan Freeman", "Bob Gunton"}, "Drank Darabont", "1994", "Crime drama", 1);
     EXPECT_EQ(m.getTitle(), "The Shawshank Redemption");
 }
@@ -66,5 +67,32 @@ TEST(MovieTest, getID) {
     Movie m2("Star Wars: A New Hope", {"Mark Hamill", "Harrison Ford", "Carrie Fisher", "Peter Cushing", "Alec Guinness"}, "George Lucas", "1977", "Science-Fiction", 2);
     EXPECT_EQ(m.getID(), 2);
 }
+//----------------------------------------------------------------------------------------------------------------------------
+TEST(MovieTest, getTitle) {
+    Movie m3("John Wick", {"Keanu Reeves", "Michael Nyqvist", "Alfie Allen", "Willem Dafoe", "Adrianne Palicki"}, "Chad Stahelski", "2014", "Action", 3);
+    EXPECT_EQ(m.getTitle(), "John Wick");
+}
+TEST(MovieTest, getActors) {
+    Movie m3("John Wick", {"Keanu Reeves", "Michael Nyqvist", "Alfie Allen", "Willem Dafoe", "Adrianne Palicki"}, "Chad Stahelski", "2014", "Action", 3);
+    std::vector<std::string> expectedActors = {"Keanu Reeves", "Michael Nyqvist", "Alfie Allen", "Willem Dafoe", "Adrianne Palicki"};
+    EXPECT_EQ(m.getActors(), expectedActors);
+}
+TEST(MovieTest, getDirector) {
+    Movie m3("John Wick", {"Keanu Reeves", "Michael Nyqvist", "Alfie Allen", "Willem Dafoe", "Adrianne Palicki"}, "Chad Stahelski", "2014", "Action", 3);
+    EXPECT_EQ(m.getTitle(), "Chad Stahelski");
+}
+TEST(MovieTest, getReleaseDate) {
+    Movie m3("John Wick", {"Keanu Reeves", "Michael Nyqvist", "Alfie Allen", "Willem Dafoe", "Adrianne Palicki"}, "Chad Stahelski", "2014", "Action", 3);
+    EXPECT_EQ(m.getTitle(), "2014");
+}
+TEST(MovieTest, getGenre) {
+    Movie m3("John Wick", {"Keanu Reeves", "Michael Nyqvist", "Alfie Allen", "Willem Dafoe", "Adrianne Palicki"}, "Chad Stahelski", "2014", "Action", 3);
+    EXPECT_EQ(m.getGenre(), "Action");
+}
+TEST(MovieTest, getID) {
+    Movie m3("John Wick", {"Keanu Reeves", "Michael Nyqvist", "Alfie Allen", "Willem Dafoe", "Adrianne Palicki"}, "Chad Stahelski", "2014", "Action", 3);
+    EXPECT_EQ(m.getID(), 3);
+}
+//----------------------------------------------------------------------------------------------------------------------------
 
 
