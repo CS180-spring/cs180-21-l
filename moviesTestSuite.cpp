5db738d8242d64f008fca6140418c33333499117
ian <77030032+yanathan@users.noreply.github.com>
2023-05-01 09:47:44 -0700

    Update moviesTestSuite.cpp

--- moviesTestSuite.cpp
+++ moviesTestSuite.cpp
@@ -5,32 +5,34 @@
 #include "Person.h"
 // Tests
 TEST(MovieTest, getTitle) {
-    Movie m("The Godfather", {"Marlon Brando", "Al Pacino"}, "Francis Ford Coppola", "1972", "Crime drama", 1);
-    EXPECT_EQ(m.getTitle(), "The Godfather");
+    Movie m("The Shawshank Redemption", {"Tim Robbins", "Andy Dufresne"}, "Drank Darabont", "1994", "Crime drama", 1);
+    EXPECT_EQ(m.getTitle(), "The Shawshank Redemption");
 }
 
 TEST(MovieTest, getActors) {
-    Movie m("The Godfather", {"Marlon Brando", "Al Pacino"}, "Francis Ford Coppola", "1972", "Crime drama", 1);
-    std::vector<std::string> expectedActors = {"Marlon Brando", "Al Pacino"};
+    Movie m("The Shawshank Redemption", {"Tim Robbins", "Andy Dufresne"}, "Drank Darabont", "1994", "Crime drama", 1);
+    std::vector<std::string> expectedActors = {"Tim Robbins", "Andy Dufresne"};
     EXPECT_EQ(m.getActors(), expectedActors);
 }
 
 TEST(MovieTest, getDirector) {
-    Movie m("The Godfather", {"Marlon Brando", "Al Pacino"}, "Francis Ford Coppola", "1972", "Crime drama", 1);
-    EXPECT_EQ(m.getDirector(), "Francis Ford Coppola");
+    Movie m("The Shawshank Redemption", {"Tim Robbins", "Andy Dufresne"}, "Drank Darabont", "1994", "Crime drama", 1);
+    EXPECT_EQ(m.getDirector(), "Drank Darabont");
 }
 
 TEST(MovieTest, getReleaseDate) {
-    Movie m("The Godfather", {"Marlon Brando", "Al Pacino"}, "Francis Ford Coppola", "1972", "Crime drama", 1);
-    EXPECT_EQ(m.getReleaseDate(), "1972");
+    Movie m("The Shawshank Redemption", {"Tim Robbins", "Andy Dufresne"}, "Drank Darabont", "1994", "Crime drama", 1);
+    EXPECT_EQ(m.getReleaseDate(), "1994");
 }
 
 TEST(MovieTest, getGenre) {
-    Movie m("The Godfather", {"Marlon Brando", "Al Pacino"}, "Francis Ford Coppola", "1972", "Crime drama", 1);
+    Movie m("The Shawshank Redemption", {"Tim Robbins", "Andy Dufresne"}, "Drank Darabont", "1994", "Crime drama", 1);
     EXPECT_EQ(m.getGenre(), "Crime drama");
 }
 
 TEST(MovieTest, getID) {
-    Movie m("The Godfather", {"Marlon Brando", "Al Pacino"}, "Francis Ford Coppola", "1972", "Crime drama", 1);
+    Movie m("The Shawshank Redemption", {"Tim Robbins", "Andy Dufresne"}, "Drank Darabont", "1994", "Crime drama", 1);
     EXPECT_EQ(m.getID(), 1);
 }
+
+
