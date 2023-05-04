2b4d59e3daef063cde1dd3d5f61140ad1bdb9d4f
ian <77030032+yanathan@users.noreply.github.com>
2023-05-04 12:59:25 -0700

    Updated personTestSuite.cpp

--- personTestSuite.cpp
+++ personTestSuite.cpp
@@ -1,36 +1,93 @@
+#include "pch.h"
 #include <gtest/gtest.h>
 #include <string>
 #include <vector>
-#include "Movie.h"
-#include "Person.h"
+
+#include "../Person.h"
 
 
-// Tests for Person
-TEST(PersonTest, getFullName) {
-    //(first name, last name, {movieActedIn1, movieActedIn2}, {movieDirected1}, age)
-    Person p("Marlon", "Brando", {"The Godfather", "Apocalypse Now"}, {"One-Eyed Jacks"}, 80);
-    EXPECT_EQ(p.getFullName(), "Marlon Brando");
+TEST(PersonTest, getDOB1) {
+    Person p("Leonardo DiCaprio", "November 11, 1974", { "Titanic", "The Wolf of Wall Street", "The Great Gatsby", "Django Unchained" }, { "The Wolf of Wall Street", "Delirium", "Red Riding Hood" });
+    EXPECT_EQ(p.getDOB(), "November 11, 1974");
+
 }
 
-TEST(PersonTest, getMoviesActedIn) {
-    Person p("Marlon", "Brando", {"The Godfather", "Apocalypse Now"}, {"One-Eyed Jacks"}, 80);
-    std::vector<std::string> expectedMoviesActedIn = {"The Godfather", "Apocalypse Now"};
-    EXPECT_EQ(p.getMoviesActedIn(), expectedMoviesActedIn);
+
+TEST(PersonTest, getMoviesStarred1) {
+    Person p("Leonardo DiCaprio", "November 11, 1974", { "Titanic", "The Wolf of Wall Street", "The Great Gatsby", "Django Unchained" }, { "The Wolf of Wall Street", "Delirium", "Red Riding Hood" });
+    vector<string> expectedMoviesStarred = { "Titanic", "The Wolf of Wall Street", "The Great Gatsby", "Django Unchained" };
+    EXPECT_EQ(p.getMoviesStarred(), expectedMoviesStarred);
+
 }
 
-TEST(PersonTest, getCharactersPlayed) {
-    Person p("Robert", "Downey Jr.", {"Iron Man", "The Avengers"}, {"None"}, 57);
-    std::vector<std::string> expectedCharactersPlayed = {"Tony Stark"};
-    EXPECT_EQ(p.getCharactersPlayed(), expectedCharactersPlayed);
-}
-
-TEST(PersonTest, getMoviesDirected) {
-    Person p("Marlon", "Brando", {"The Godfather", "Apocalypse Now"}, {"One-Eyed Jacks"}, 80);
-    std::vector<std::string> expectedMoviesDirected = {"One-Eyed Jacks"};
+TEST(PersonTest, getMoviesDirected1) {
+    Person p("Leonardo DiCaprio", "November 11, 1974", { "Titanic", "The Wolf of Wall Street", "The Great Gatsby", "Django Unchained" }, { "The Wolf of Wall Street", "Delirium", "Red Riding Hood" });
+    vector<string> expectedMoviesDirected = { "The Wolf of Wall Street", "Delirium", "Red Riding Hood" };
     EXPECT_EQ(p.getMoviesDirected(), expectedMoviesDirected);
+
+}
+//---------------------------------------------------------------------------------------------------------------------------
+
+TEST(PersonTest, getName2) {
+    Person p("Keanu Reeves", "September 2, 1964", { "Toy Story 4", "John Wick", "The Matrix", "John Wick: Chapter 2", "John Wick: Chapter 3 - Parabellum", "John Wick: Chapter 4" }, { "Man of Tai Chi" });
+    EXPECT_EQ(p.getName(), "Keanu Reeves");
+
 }
 
-TEST(PersonTest, getAge) {
-    Person p("Marlon", "Brando", {"The Godfather", "Apocalypse Now"}, {"One-Eyed Jacks"}, 80);
-    EXPECT_EQ(p.getAge(), "80");
+TEST(PersonTest, getDOB2) {
+    Person p("Keanu Reeves", "September 2, 1964", { "Toy Story 4", "John Wick", "The Matrix", "John Wick: Chapter 2", "John Wick: Chapter 3 - Parabellum", "John Wick: Chapter 4" }, { "Man of Tai Chi" });
+    EXPECT_EQ(p.getDOB(), "September 2, 1964");
+
 }
+
+TEST(PersonTest, getmoviesStarred2) {
+    Person p("Keanu Reeves", "September 2, 1964", { "Toy Story 4", "John Wick", "The Matrix", "John Wick: Chapter 2", "John Wick: Chapter 3 - Parabellum", "John Wick: Chapter 4" }, { "Man of Tai Chi" });
+    vector<string> expectedMoviesStarred = { "Toy Story 4", "John Wick", "The Matrix", "John Wick: Chapter 2", "John Wick: Chapter 3 - Parabellum", "John Wick: Chapter 4" };
+    EXPECT_EQ(p.getMoviesStarred(), expectedMoviesStarred);
+
+}
+
+TEST(PersonTest, getmoviesDirected2) {
+    Person p("Keanu Reeves", "September 2, 1964", { "Toy Story 4", "John Wick", "The Matrix", "John Wick: Chapter 2", "John Wick: Chapter 3 - Parabellum", "John Wick: Chapter 4" }, { "Man of Tai Chi" });
+    vector<string> expectedMoviesDirected = { "Man of Tai Chi" };
+    EXPECT_EQ(p.getMoviesDirected(), expectedMoviesDirected);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
