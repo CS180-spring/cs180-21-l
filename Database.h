d653ca07f62e3a0da3211f67aff3b2a03a318103
rrchandler <56704293+rrchandler@users.noreply.github.com>
2023-05-06 23:53:33 -0700

    Added Database class and File class source files

---
+++ Database.h
@@ -0,0 +1,56 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+#include "Movie.h"
+#include "Person.h"
+using namespace std;
+
+class Database {
+public:
+    //Converts whatever is the in the file to an unordered_map for movies and people
+    //Change function types and parameters as needed
+    void loadMovies();
+    void loadPeople();
+
+    //Converts the 2 unordered_maps to JSON format
+    void storeMovies(unordered_map<unsigned int, Movie>) const;
+    void storePeople(unordered_map<unsigned int, Person>) const;
+
+    
+    //Ryan Chandler will do these:
+    void buildDatabase();
+
+    //building the movie queries
+    void buildTitleIndex();
+    void buildGenreIndex();
+    void buildRatingIndex();
+    void buildReleaseDateIndex();
+
+    //building the person queries
+    void buildNameIndex();
+    void buildDOBIndex();
+
+private:
+    //The unique ID is mapped to the Movie/Person object
+    unordered_map<unsigned int, Movie> movies;
+    unordered_map<unsigned int, Person> people;
+
+    //Movie Queries
+    unordered_multimap<string, unsigned int> titleIndex;
+    unordered_multimap<string, unsigned int> genreIndex;
+    unordered_multimap<double, unsigned int> ratingIndex;
+    unordered_multimap<string, unsigned int> releaseDateIndex;
+    //Movies a person starred in will be stored in the Person object or taken from a JSON
+
+    //Person Queries
+    unordered_multimap<string, unsigned int> nameIndex;
+    unordered_multimap<string, unsigned int> dobIndex;
+    //People who starred in a particular movie will be stored in the Movie object or taken from a JSON
+
+};
+
+#endif
---
+++ File.h
@@ -0,0 +1,26 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <fstream>
+#include "Database.h"
+
+using namespace std;
+
+//Per Nick, there will be 4 JSON files
+//To be more Agile, we will only focus on 2 for now: Movies.JSON and People.JSON
+//Get load and save done by the start of Monday's lab
+
+
+class File {
+public:
+    void saveDatabase(const string& movieFile, const string& personFile, const Database& db);
+        //db.storeMovies
+        //db.storePeople
+
+
+    void loadDatabase(const std::string& movieFile, const string& personFile, Database& db);
+        //db.loadMovies
+        //db.loadPeople
+};
+
+#endif
